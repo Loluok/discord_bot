6a359392ef5ddc693f37c04e141e028031dcf281
Aldo Aliscioni <aldoaliscioni18@gmail.com>
2025-01-20 13:03:57 -0300

    added the posibility to add person to the whitelist in the minecraft server via the discord bot

--- commands/index.js
+++ commands/index.js
@@ -17,6 +17,7 @@ import getPat from "./pat.js";
 import getKiss from "./kiss.js";
 import getRun from "./run.js";
 import getSad from "./sad.js";
+import getWhitelist from "./whitelist.js";
 
 const commands = {
   help: getHelp,
@@ -39,6 +40,9 @@ const commands = {
   kiss: getKiss,
   run: getRun,
   sad: getSad,
+  wh: getWhitelist,
+  wl: getWhitelist,
+  whitelist: getWhitelist,
 };
 
 export default commands;
---
+++ commands/whitelist.js
@@ -0,0 +1,33 @@
+import { exec } from "child_process";
+
+const possibleWhitelistCommands = ["add", "remove"]
+
+export default async function getWhitelist(message, messageContent) {
+  const [whitelistCommand, player] = messageContent;
+  if (!possibleWhitelistCommands.includes(whitelistCommand)) {
+    return message.reply(
+      `${whitelistCommand} no se reconoce como un comando de whitelist. \nIntenta "add" o "remove".`
+    );
+  }
+
+  if (!player) {
+    return message.reply("Por favor, especifica a un jugador");
+  }
+
+  const command = `mcrcon -H 127.0.0.1 -P 25575 -p "${process.env.SERVER_RCON_PASSWORD}" "whitelist ${whitelistCommand} ${player}"`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error al ejecutar el comando: ${error.message}`);
+      return message.reply("Hubo un error al intentar ejecutar el comando.");
+    }
+
+    if (stderr) {
+      console.error(`Error en stderr: ${stderr}`);
+      return message.reply("Hubo un problema con el comando.");
+    }
+
+    console.log(`stdout: ${stdout}`);
+    return message.reply(`${stdout}`);
+  });
+}
